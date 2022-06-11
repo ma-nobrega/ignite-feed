9bd420044b7b7054ade9ad3e453337b138d42f58
ma-nobrega <matheus.adao.nobrega@gmail.com>
2022-06-10 21:49:23 -0300

    add css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post"
+import { Post } from "./Post";
 
-import './styles.css';
+import "./global.css";
 
 export function App() {
   return (
@@ -16,5 +16,5 @@ export function App() {
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam iste fuga facere omnis corrupti atque ex impedit, sunt hic rerum placeat, eaque nam repudiandae sed culpa iure, id eius aspernatur."
       />
     </>
-  )
+  );
 }
