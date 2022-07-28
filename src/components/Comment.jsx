a970ae79eb139247e2e6d6f5c37fa7a6e08b74e7
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2022-07-28 13:52:01 -0300

    add clapping method

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,15 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import { Avatar } from './Avatar';
-import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
 export function Comment({ content, onDeleteComment }) {
-  function handleDeleteComment(){
+  const [likeCount, setLikeCount] = useState(0);
+  function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -14,21 +20,23 @@ export function Comment({ content, onDeleteComment }) {
           <header>
             <div className={styles.authorAndTime}>
               <strong>Matheus Nobrega</strong>
-              <time title="11 de maio as 08:13h" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+              <time title="11 de maio as 08:13h" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
             </div>
-            <button  onClick={handleDeleteComment} title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
     </div>
-  )
+  );
 }
